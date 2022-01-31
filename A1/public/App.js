96847c71d9edaf8ba592bdedfa1d1bdd4ed04cb1
Jacob Jin <yjin7099@sdsu.edu>
2022-01-30 19:18:51 -0800

    Add A1

---
+++ A1/public/App.js
@@ -0,0 +1,8 @@
+const element = /*#__PURE__*/React.createElement("div", {
+  title: "Outer div"
+}, /*#__PURE__*/React.createElement("h1", null, "Jacob (Yage) Jin"), /*#__PURE__*/React.createElement("img", {
+  src: "assets/profile.jpg"
+}), /*#__PURE__*/React.createElement("p", null, "Hi there, my name is Yage Jin, and I go by Jacob. I am a 2nd year master student at SDSU, and I am expecting to graduate in May this year. I am looking forward to learn more about the MERN stack and full stack web development from this course!"), /*#__PURE__*/React.createElement("a", {
+  href: ""
+}, "VIEW MY GITHUB REPO"));
+ReactDOM.render(element, document.getElementById('contents'));
---
+++ A1/src/App.jsx
@@ -0,0 +1,13 @@
+const element = (
+  <div title="Outer div">
+    <h1>Jacob (Yage) Jin</h1>
+    <img src="assets/profile.jpg"></img>
+    <p>Hi there, my name is Yage Jin, and I go by Jacob. I am a 2nd year master
+    student at SDSU, and I am expecting to graduate in May this year. I am looking
+    forward to learn more about the MERN stack and full stack web development
+    from this course!</p>
+    <a href="">VIEW MY GITHUB REPO</a>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById('contents'));
